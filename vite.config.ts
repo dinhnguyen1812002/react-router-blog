8a7a8faf9e18ffc7c05952b8ad3e024815ece50c
dinhnguyen <dinhnguyen1812002@gmail.com>
2026-01-21 22:16:21 +0700

    feat: add visualizer to analyze bundle size

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,44 @@
+import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+
   define: {
-    global: 'globalThis',
+    global: "globalThis",
   },
 
-  optimizeDeps: {
-    include: ['@uiw/react-markdown-preview'],
-  },
   ssr: {
-    noExternal: ['@uiw/react-markdown-preview'],
+    external: [
+      "@uiw/react-markdown-preview",
+      "@uiw/react-md-editor",
+      "@tiptap/react",
+      "@tiptap/starter-kit",
+      "@tiptap/extensions",
+      "@tiptap/pm",
+      "lowlight",
+      "recharts",
+      "motion",
+      "@lottiefiles/dotlottie-react",
+    ],
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react-router")) return "router";
+            if (id.includes("react")) return "react";
+            if (id.includes("radix")) return "radix";
+            if (id.includes("tiptap")) return "editor";
+            if (id.includes("recharts")) return "charts";
+            return "vendor";
+          }
+        },
+      },
+    },
   },
 });
